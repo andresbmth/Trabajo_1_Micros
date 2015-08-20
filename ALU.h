703668b56b8fdfdc6742a1360fbc2a196f8f5031
test <a@b.com>
2015-08-20 13:03:18 -0500

    se añadio la libreria ALU.h

---
+++ ALU.h
@@ -0,0 +1,5 @@
+void AND(long int *R0,long int R1,long int R2);
+void EOR(long int *R0,long int R1,long int R2);
+void MOV(long int *R0,long int R1,long int R2);
+void ORR(long int *R0,long int R1,long int R2);
+void SUB(long int *R0,long int R1,long int R2);
