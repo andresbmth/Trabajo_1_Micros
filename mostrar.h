008c01d2ed33841c4b02a3818f394fc18dcdc442
brayanpotes <bagp_0713@hotmail.com>
2015-08-21 18:22:17 -0500

    se agrego la sintaxos del archivo mostrar.h para la documentacion

--- mostrar.h
+++ mostrar.h
@@ -1,1 +1,6 @@
-void mostrar_valores(long int valores[]);
+/**
+* \brief funcion para mostrar los datos del registro
+* \param registros imprime los valores de los registros 
+*/
+
+void mostrar_valores(unsigned long int registros[]);
