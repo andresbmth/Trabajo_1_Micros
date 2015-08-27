0f5e9b3a1baa78c1f9f95e9b9f4d1bec8baada02
brayanpotes <bagp_0713@hotmail.com>
2015-08-27 12:43:04 -0500

    modificacion del alu.h para iniciar proceso de las banderas

--- ALU.h
+++ ALU.h
@@ -1,46 +1,64 @@
 /**
+* \brief Funcion MOV
+* \param *Rd puntero del registro de destino
+* \param Rn valor de registro orgien
+* \param *R_Banderas registro de las banderas
+*/ 
+void Banderas(uint32_t *Rd, uint32_t Rn, uint32_t Rm, uint32_t,char R_Banderas);
+
+
+/**
 * \brief Funcion ADD
 * \param *Rd puntero del primer registro
 * \param Rn valor del segundo registro
 * \param Rm valor del tercer registro
+* \param *R_Banderas registro de las banderas
 */
-void ADD(uint32_t *Rd,uint32_t Rn,uint32_t Rm);
+void ADD(uint32_t *Rd,uint32_t Rn,uint32_t Rm,char *R_Banderas);
 
 /**
 * \brief Funcion AND
 * \param *Rd puntero del primer registro
 * \param Rn valor del segundo registro
 * \param Rm valor del tercer registro
+* \param *R_Banderas registro de las banderas
 */
-void AND(uint32_t *Rd,uint32_t Rn,uint32_t Rm);
+void AND(uint32_t *Rd,uint32_t Rn,uint32_t Rm,char *R_Banderas);
 
 /**
 * \brief Funcion EOR
 * \param *Rd puntero del primer registro
 * \param Rn valor del segundo registro
 * \param Rm valor del tercer registro
+* \param *R_Banderas registro de las banderas
 */
-void EOR(uint32_t *Rd,uint32_t Rn,uint32_t Rm);
+void EOR(uint32_t *Rd,uint32_t Rn,uint32_t Rm,char *R_Banderas);
+
+
+
 
 /**
 * \brief Funcion MOV
 * \param *Rd puntero del registro de destino
 * \param Rn valor de registro orgien
+* \param *R_Banderas registro de las banderas
 */
-void MOV(uint32_t *Rd,uint32_t Rn);
+void MOV(uint32_t *Rd,uint32_t Rn,char *R_Banderas);
 
 /**
 * \brief Funcion ORR
 * \param *Rd puntero del primer registro
 * \param Rn valor del segundo registro
 * \param Rm valor del tercer registro
+* \param *R_Banderas registro de las banderas
 */
-void ORR(uint32_t *Rd,uint32_t Rn,uint32_t Rm);
+void ORR(uint32_t *Rd,uint32_t Rn,uint32_t Rm,char *R_Banderas);
 
 /**
 * \brief Funcion SUB
 * \param *Rd puntero del primer registro
 * \param Rn valor del segundo registro
 * \param Rm valor del tercer registro
+* \param *R_Banderas registro de las banderas
 */
-void SUB(uint32_t *Rd,uint32_t Rn,uint32_t Rm);
+void SUB(uint32_t *Rd,uint32_t Rn,uint32_t Rm,char *R_Banderas);
