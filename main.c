d1756df3e0b045af1c808af05201e3e6f16ce3f4
brayanpotes <bagp_0713@hotmail.com>
2015-09-08 02:42:01 -0500

    avances en la interfaz grafica

--- main.c
+++ main.c
@@ -3,10 +3,41 @@
 #include <stdint.h>
 #include "mostrar.h"
 #include "ALU.h"
+#include "curses.h"
+#include "desplazamiento.h"
 
-int main()
+int main(void)
 {
-    uint32_t registro[12]={0,0,0,0,0,0,0,0,0,0,0,0};
+	uint32_t registro[12];
+	char R_bandera[4];
+	initscr();		/* Inicia modo curses */
+	curs_set(0);	/* Cursor Invisible */
+	raw();			/* Activa modo raw */
+	keypad(stdscr, TRUE);	/* Obtener F1, F2, etc */
+	noecho();		/* No imprimir los caracteres leidos */
+    start_color();	/* Permite manejar colores */
+	init_pair(1, COLOR_MAGENTA, COLOR_BLACK);	/* Pair 1 -> Texto verde fondo Negro */
+    init_pair(2,COLOR_YELLOW,COLOR_BLACK);
+	
+	
+	
+	registro[0]=11;
+    registro[1]=13245345;
+	registro[2]=2345986;
+    attron(COLOR_PAIR(1));  /* Activa el color morado para el texto y negro para el fondo Pair 1*/
+	
+    ADD(&registro[0],registro[1],registro[2],R_bandera);
+	move(6,17);
+    attron(COLOR_PAIR(1));
+    printw("Resultados obtenidos de las operaciones ADD=\n",registro[0]);
+    refresh();
+    getch();
+    endwin();
+
+
+	
+	
+    //uint32_t registro[12]={0,0,0,0,0,0,0,0,0,0,0,0};
     mostrar_valores(registro);
 	return 0;
 }
